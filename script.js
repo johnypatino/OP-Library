6716f661d41854960090ca57cda99237d7d6af84
Jonathan <johnypatino99@gmail.com>
2023-10-04 01:25:20 -0400

    intial files + js

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+const div = document.querySelector('#container');
+const button = document.getElementById('addBook');
+
+function Book() {
+
+}
+function addBookToLibrary() {
+
+}
+
+button.addEventListener('click', addBookToLibrary);
